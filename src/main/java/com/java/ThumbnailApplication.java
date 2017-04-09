413aea9a26eca95e00d436ddce82677d78853219
xu kangfeng <xu kangfeng>
2017-04-09 22:40:06 +0800

    Initial commit

---
+++ src/main/java/com/java/ThumbnailApplication.java
@@ -0,0 +1,12 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbnailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThumbnailApplication.class, args);
+	}
+}
